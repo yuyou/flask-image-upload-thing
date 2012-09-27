d73fb8c8a4859019265e62f27e168a8bb09abfd5
Oskari Hiltunen <git@loethr.net>
2012-09-27 11:10:01 +0300

    Version 0.1.3.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class PyTest(Command):
 
 setup(
     name='Flask-Uploads',
-    version='0.1.2',
+    version='0.1.3',
     url='http://github.com/FelixLoether/flask-uploads',
     author='Oskari Hiltunen',
     author_email='flask-uploads@loethr.net',
